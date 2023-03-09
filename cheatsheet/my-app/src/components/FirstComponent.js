c0245fc4e10eb100bc382084c96c76a2a3c7aaa1
Alberto <guzmanmorenoalberto@hotmail.com>
2023-03-09 19:12:31 +0100

    Inicio de React

---
+++ cheatsheet/my-app/src/components/FirstComponent.js
@@ -0,0 +1,9 @@
+export default function FirstComponent() {
+  return (
+    <div>
+        <h1>Titulo</h1>
+        <small>9 de Marzo de 2023</small>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum deserunt repellat omnis voluptatem.</p>
+    </div>
+  )
+}
