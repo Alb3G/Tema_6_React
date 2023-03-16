b8efea3561979140e2246927cbc27d1ce3799eb5
Alberto <guzmanmorenoalberto@hotmail.com>
2023-03-16 19:07:09 +0100

    Ejercicio 2 terminado

--- ejercicios/ejercicio2/src/components/List.js
+++ ejercicios/ejercicio2/src/components/List.js
@@ -1,8 +1,10 @@
-export default function List (props) {
-  const listItems = props.products.map(item => <li key=''>{item.product}</li>)
+export default function List ({ category, products }) {
+  // En lugar de pasarle las props como paramentro lo pasamos como un obj completo con sus propiedades para desestructurarlo
+  // y que asi el codigo final quede mas limpio y legible. La Key tiene que ser algo unico que nunca se repita
+  const listItems = products.map(({ id, product, precio }) => <li key={id}>Id:{id} - {product} - price: {precio}$</li>)
   return (
     <>
-      <h1>{props.category}</h1>
+      <h1>{category}</h1>
       <ol>
         {listItems}
       </ol>
