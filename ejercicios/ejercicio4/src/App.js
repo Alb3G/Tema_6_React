726bdf841d5ce9433a61c78764764396d9acc080
Alberto Guzman Moreno <albertoguzman@MacBook-Pro-de-Alberto.local>
2023-04-02 13:29:34 +0200

    Ejercicio4 in progress

--- ejercicios/ejercicio4/src/App.js
+++ ejercicios/ejercicio4/src/App.js
@@ -1,8 +1,23 @@
 import './App.css'
+import ToDos from './components/ToDos'
+import { useEffect } from 'react'
+import Input from './components/Input'
 
 function App () {
+  const URL = 'https://jsonplaceholder.typicode.com/todos'
+  useEffect(() => {
+    fetchData()
+  }, [URL])
+  function fetchData () {
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => data.slice(0, 20).forEach(element => console.log(element)))
+  }
   return (
-    <div className='App' />
+    <div className='App'>
+      <Input />
+      <ToDos />
+    </div>
   )
 }
 
---
+++ ejercicios/ejercicio4/src/components/Input.js
@@ -0,0 +1,8 @@
+export default function Input () {
+  return (
+    <div className='container'>
+      <h1 className='d-flex'>To-Do List</h1>
+      <input type='text' className='form-control' />
+    </div>
+  )
+}
