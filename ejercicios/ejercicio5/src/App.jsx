0fc442fe7e5cf7da91291ddf418a2d14856c0ca5
Alberto <guzmanmorenoalberto1998@gmail.com>
2023-04-25 12:23:42 +0200

    Empezado ejercicio 6

--- ejercicios/ejercicio5/src/App.jsx
+++ ejercicios/ejercicio5/src/App.jsx
@@ -1,30 +1,36 @@
 import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom'
+import { createContext, useState } from 'react'
 import './index.css'
 import Ejercicio1 from './routes/Ejercicio1'
 import Ejercicio2 from './routes/Ejercicio2'
 import Ejercicio3 from './routes/Ejercicio3'
 import Ejercicio4 from './routes/Ejercicio4'
 
+export const StateContext = createContext({})
+
 function App () {
+  const [toDos, setToDos] = useState([])
   return (
     <>
-      <BrowserRouter>
-        <nav className='navbar bg-body-tertiary bg-warning mb-3'>
-          <div className='container-fluid'>
-            <NavLink to='/Ejercicio1' className='navbar-brand linksColor'>Ejercicio1</NavLink>
-            <NavLink to='/Ejercicio2' className='navbar-brand linksColor'>Ejercicio2</NavLink>
-            <NavLink to='/Ejercicio3' className='navbar-brand linksColor'>Ejercicio3</NavLink>
-            <NavLink to='/Ejercicio4' className='navbar-brand linksColor'>Ejercicio4</NavLink>
-          </div>
-        </nav>
+      <StateContext.Provider value={[toDos, setToDos]}>
+        <BrowserRouter>
+          <nav className='navbar bg-body-tertiary bg-warning mb-3'>
+            <div className='container-fluid'>
+              <NavLink to='/Ejercicio1' className='navbar-brand linksColor'>Ejercicio1</NavLink>
+              <NavLink to='/Ejercicio2' className='navbar-brand linksColor'>Ejercicio2</NavLink>
+              <NavLink to='/Ejercicio3' className='navbar-brand linksColor'>Ejercicio3</NavLink>
+              <NavLink to='/Ejercicio4' className='navbar-brand linksColor'>Ejercicio4</NavLink>
+            </div>
+          </nav>
 
-        <Routes>
-          <Route path='/Ejercicio1' element={<Ejercicio1 />} />
-          <Route path='/Ejercicio2' element={<Ejercicio2 />} />
-          <Route path='/Ejercicio3' element={<Ejercicio3 />} />
-          <Route path='/Ejercicio4' element={<Ejercicio4 />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path='/Ejercicio1' element={<Ejercicio1 />} />
+            <Route path='/Ejercicio2' element={<Ejercicio2 />} />
+            <Route path='/Ejercicio3' element={<Ejercicio3 />} />
+            <Route path='/Ejercicio4' element={<Ejercicio4 />} />
+          </Routes>
+        </BrowserRouter>
+      </StateContext.Provider>
     </>
   )
 }
--- ejercicios/ejercicio5/src/routes/Ejercicio4.jsx
+++ ejercicios/ejercicio5/src/routes/Ejercicio4.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import useFetch from '../hooks/useFetch'
 import Input from '../components/Input'
 import Todos from '../components/Todos'
+import { StateContext } from '../App'
 
 function App () {
-  const [toDos, setToDos] = useState([])
+  const [toDos, setToDos] = useContext(StateContext)
 
   const URL = 'https://jsonplaceholder.typicode.com/todos'
   useFetch(URL, setToDos)
