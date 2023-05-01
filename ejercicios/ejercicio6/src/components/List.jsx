d1438aee144b750511b3e963a54121a801ad021b
Alberto <guzmanmorenoalberto1998@gmail.com>
2023-05-01 17:21:53 +0200

    Modificacion visual ejercicio 2

--- ejercicios/ejercicio6/src/components/List.jsx
+++ ejercicios/ejercicio6/src/components/List.jsx
@@ -1,10 +1,10 @@
 export default function List ({ name, products }) {
   return (
     <div>
-      <ul className='list-group mb-3'>
+      <ul className='list-group w-25 p-0 container mb-3'>
         <h1 className='list-group-item active'>{name}</h1>
         {products.map(({ id, product, price }) => {
-          return <li key={id} className='list-group-item'><strong>Num:</strong> {id} - {product} - {price}</li>
+          return <li key={id} className='list-group-item d-flex justify-content-center'><strong>Num:</strong> {id} - {product} - {price}</li>
         })}
       </ul>
     </div>
