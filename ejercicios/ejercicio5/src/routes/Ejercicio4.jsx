2a5a96c489e3f95606a379c731defdd4a7b71d92
Alberto <guzmanmorenoalberto1998@gmail.com>
2023-05-02 16:50:18 +0200

    Arreglado ejercicio 5

--- ejercicios/ejercicio5/src/routes/Ejercicio4.jsx
+++ ejercicios/ejercicio5/src/routes/Ejercicio4.jsx
@@ -1,11 +1,10 @@
-import { useState, useContext } from 'react'
+import { useState } from 'react'
 import useFetch from '../hooks/useFetch'
 import Input from '../components/Input'
 import Todos from '../components/Todos'
-import { StateContext } from '../App'
 
 function App () {
-  const [toDos, setToDos] = useContext(StateContext)
+  const [toDos, setToDos] = useState([])
 
   const URL = 'https://jsonplaceholder.typicode.com/todos'
   useFetch(URL, setToDos)
